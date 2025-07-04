a23201adb11ea487369a89260106208e3d8b9541
Arthur Ferreira <tutudacosta996@gmail.com>
2025-07-04 20:33:24 -0300

    online bot

---
+++ index.js
@@ -0,0 +1,18 @@
+import { Client, IntentsBitField } from "discord.js";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ]
+});
+
+client.on('ready', (e) => {
+    console.log(`${e.user.tag} está online!`);
+});
+
+client.login(process.env.TOKEN);
