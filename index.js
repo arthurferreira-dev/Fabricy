5ec6b36323d02f0b84a8300bb72c4ae77606d306
Arthur Ferreira <tutudacosta996@gmail.com>
2025-07-04 21:59:13 -0300

    quiet

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
-import { Client, IntentsBitField } from "discord.js";
-import * as dotenv from "dotenv";
-dotenv.config();
+const Discord = require("discord.js");
 
-const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
+require('dotenv').config();
+
+const client = new Discord.Client({
+    intents: [1, 512, 32768, 2, 128,
+        Discord.IntentsBitField.Flags.Guilds,
+        Discord.IntentsBitField.Flags.GuildMembers,
+        Discord.IntentsBitField.Flags.GuildMessages,
+        Discord.IntentsBitField.Flags.MessageContent,
+        Discord.IntentsBitField.Flags.GuildMessageReactions,
+        Discord.IntentsBitField.Flags.DirectMessages,
+        Discord.IntentsBitField.Flags.GuildInvites
+    ],
+    partials: [
+        Discord.Partials.User,
+        Discord.Partials.Message,
+        Discord.Partials.Reaction,
+        Discord.Partials.GuildMember,
+        Discord.Partials.Channel
     ]
 });
+
+module.exports = client;
 
 client.on("messageCreate", (message) => {
     if (message.author.bot) return
 
-    if (message.content ===  "!help") {
-        message.reply("Vá para #𝘿𝙪𝙫𝙞𝙙𝙖s-❓")
+    if (message.content ===  '!help') {
+        message.reply('Vá para #𝘿𝙪𝙫𝙞𝙙𝙖s-❓')
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.author.bot) return
+
+    if (message.content === '@Fabricy') {
+        message.reply('O que você quer?')
     }
 })
 
